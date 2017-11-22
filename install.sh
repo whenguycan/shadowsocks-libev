cb4478731ddca87c149f9ea257b882c9bf876ad6
jim <jim@cit.com>
2017-11-22 09:40:50 +0800

    update

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+sh configure
+make && make install
+mkdir /etc/shadowsocks
+cp ~/shadowsocks-libev/shadowsocks.service /lib/systemd/system/shadowsocks.service
+cp ~/shadowsocks-libev/config.json /etc/shadowsocks/config.json
+systemctl enable shadowsocks.service
+firewall-cmd --add-port=20517/tcp --permanent
+firewall-cmd --reload
