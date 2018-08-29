21921882c0f415c2bc2cb609d146ad29f6b0a049
Desktop <whenguycan@163.com>
2018-08-29 22:04:02 +0800

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ make && make install
 mkdir /etc/shadowsocks
 cp ~/shadowsocks-libev/shadowsocks.service /lib/systemd/system/shadowsocks.service
 cp ~/shadowsocks-libev/config.json /etc/shadowsocks/config.json
+systemctl start shadowsocks.service
 systemctl enable shadowsocks.service
 firewall-cmd --add-port=20517/tcp --permanent
 firewall-cmd --reload
